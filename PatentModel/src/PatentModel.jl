256dd1380ff98e1586c55aac9ecc8c4b64b0cd08
SanteriVtj <santeri.vaataja@gmail.com>
2022-05-08 18:24:54 +0300

    simulation otherwise done, but hz calculation isn't complete

--- PatentModel/src/PatentModel.jl
+++ PatentModel/src/PatentModel.jl
@@ -1,39 +1,57 @@
 module PatentModel
 
+import Base.@kwdef
 export Data, params, simulate_data, smm, hello
 
-struct Data
+@kwdef struct Data
     y::Array{Float64}
-    c::Array{Real}
+    fee::Array{Real}
     sample::Array{Float64}
     T::Int
+    P::Int
 end
 
-mutable struct params
+@kwdef mutable struct params
     δ::Float64
     θ::Float64
     ϕ::Float64
-    σ::Float64
+    σ::Real
     γ::Float64
 end
 
-function simulate_data(par::params, data::Data)
+function simulate_svdata(par::params, data::Data)
     δ = par.δ
     θ = par.θ
     ϕ = par.ϕ
     σ = par.σ
     γ = par.γ
 
-    c = data.c
+    fee = data.fee
     sample = data.sample
+    T = data.T
+    P = data.P
 
     μ₁ = ϕ.^(1:T).*σ.*(1 - γ)
     σ²₁ = μ₁.^2
 
-    μ = 2*log.()
-    σ² = 
+    μ = 2*log.(μ₁)-1/2*log.(σ²₁+μ₁.^2)
+    σ² = -2*log.(μ₁)+log.(σ²₁+μ₁.^2)
 
-    Nₛ = sample 
+    N = sample*σ²'.+μ'
+
+    r = zeros(size(N))
+    r[:,1] = N[:,1]
+    r̄ = zeros(size(N))
+    r̄[:,1] = Int.(N[:,1] .> fee[1])
+
+    for i in 2:T
+        r[:,i] = maximum(hcat(r[:,i-1], δ*N[:,i]))
+        r̄[:,i] = Int.(r[:,i] .> fee[i])
+    end
+
+    survive = sum(r̄, dims=1)
+    lapse = P .- survive
+    lapse_cumulative = 
 end
 
 function smm(par::params, data::Data)
